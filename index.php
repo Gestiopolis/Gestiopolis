e1f6c6c1ded835eca4c07a6a4fffe7ad475c38ac
Diego Castillo <djdiego88@gmail.com>
2014-11-27 00:59:17 -0500

    Added layout and styles to index

--- index.php
+++ index.php
@@ -1,22 +1,170 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php if (!is_user_logged_in()) { ?>
+  <!-- Empieza sección de DESTACADOS -->
+<div class="container cm8">
+  <div class="row rm8 destacados">
+    <div class="col-sm-36">
+      <div class="row rm8">
+        <div class="col-sm-9">
+          <div class="row rm8">
+            <div class="col-sm-36">
+              <div class="destacan">
+                Se destacan <i class="fa fa-angle-right"></i>
+                <span><a href="#"><i class="fa fa-plus"></i></a>
+              </div>
+            </div><!-- .col-sm-36 -->
+          </div><!-- .row -->
+          <div class="row rm8">
+            <div class="col-sm-36">
+              <article id="post-1" class="destacado-1 post">
+                <img src="http://farm6.staticflickr.com/5328/9079538062_ecac2e7887_b.jpg" alt="Reingeniería: Antecedentes y caso de éxito">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-1" href="http://www.gestiopolis.com/administracion-estrategia-2/reingenieria-antecedentes-caso-de-exito.htm" title="Reingeniería: Antecedentes y caso de éxito" rel="bookmark"><span>Reingeniería: Antecedentes y caso de éxito</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-20"><i class="fa icon-cat-20"></i></span>
+                <div class="destacado-content cat-bg-20">
+                  <div class="meta"><i class="fa fa-eye"></i> 33 <i class="fa fa-heart"></i> 6 <i class="fa fa-comment"></i> 1</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/administracion-estrategia-2/reingenieria-antecedentes-caso-de-exito.htm">En el cristianismo se habla mucho de las enseñanzas de Jesucristo, aquellas que diera hace mas de 2 mil años, algunos atribuyen que muchas de esas predicadas por Jesús de Nazaret se aplican cotidianamente, tal es el caso el mencionado en el evangelio de Juan, capítulo 3 y versículo 3 que transcribe lo que Jesús dijo “te aseguro que el que no nace de nuevo, no puede ver...</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Alan Becerra Saldaña
+                  </a>
+                  <div class="botones-compartir" id="compartir-1">
+                    <div class="platform bc-facebook" id="fb-compartir-1"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-1"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-1"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-1"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-36 -->
+          </div><!-- .row -->
+        </div><!-- .col-sm-9 -->
+        <div class="col-sm-18">
+          <div class="row rm8">
+            <div class="col-sm-18">
+              <article id="post-2" class="destacado-2 post">
+                <img src="http://farm4.staticflickr.com/3821/9077313521_71f78e3c2c_b.jpg" alt="La disciplina como llave del éxito">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-2" href="http://www.gestiopolis.com/organizacion-talento-2/disciplina-como-llave-del-exito.htm" title="La disciplina como llave del éxito" rel="bookmark"><span>La disciplina como llave del éxito</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-15"><i class="fa icon-cat-15"></i></span>
+                <div class="destacado-content cat-bg-15">
+                  <div class="meta"><i class="fa fa-eye"></i> 28 <i class="fa fa-heart"></i> 8 <i class="fa fa-comment"></i> 3</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/organizacion-talento-2/disciplina-como-llave-del-exito.htm">El éxito es la combinación de constancia, disciplina y mucha, ¡pero mucha pasión! Cuando hablamos de éxito, todos quisiéramos que de una u otra forma hubiera alguna manera rápida y fácil de alcanzarlo, afortunadamente para nosotros no existe tal camino fácil, pero el camino difícil genera en nosotros no solo el carácter del triunfador sino gozo si es que lo que...</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Roberto Celaya Figueroa
+                  </a>
+                  <div class="botones-compartir" id="compartir-2">
+                    <div class="platform bc-facebook" id="fb-compartir-2"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-2"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-2"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-2"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-18 -->
+            <div class="col-sm-18">
+              <article id="post-3" class="destacado-3 post">
+                <img src="http://farm6.staticflickr.com/5495/9077312909_73567d5e2c_b.jpg" alt="Competencias organizacionales y laborales, manual de procedimientos para su planeación e implementación en la Escuela de Hotelería y Turismo Varadero (Cuba)">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-3" href="http://www.gestiopolis.com/organizacion-talento/competencias-organizacionales-laborales-manual-procedimientos.htm" title="Competencias organizacionales y laborales, manual de procedimientos para su planeación e implementación en la Escuela de Hotelería y Turismo Varadero (Cuba)" rel="bookmark"><span>Competencias organizacionales y laborales, manual de procedimientos para su planeación e implementación en la Escuela de Hotelería y Turismo Varadero (Cuba)</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-21"><i class="fa icon-cat-21"></i></span>
+                <div class="destacado-content cat-bg-21">
+                  <div class="meta"><i class="fa fa-eye"></i> 33 <i class="fa fa-heart"></i> 15 <i class="fa fa-comment"></i> 2</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/organizacion-talento/competencias-organizacionales-laborales-manual-procedimientos.htm">Los cambios sociales, políticos y económicos de los últimos años en el mundo, convocan a un uso más racional y humano de la fuerza de trabajo, teniendo en cuenta las propias características de los trabajadores, las condiciones en que se invierte sus capacidades y los tipos de trabajo que desempeñan, pues son ellos los que esencialmente acometen las transformac...</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Ms.C. Estrella F. Acosta Corzo
+                  </a>
+                  <div class="botones-compartir" id="compartir-3">
+                    <div class="platform bc-facebook" id="fb-compartir-3"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-3"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-3"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-3"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-18 -->
+          </div><!-- .row -->
+          <div class="row rm8">
+            <div class="col-sm-36">
+              <article id="post-4" class="destacado-4 post">
+                <img src="http://farm9.staticflickr.com/8034/8070404930_61d3064fdd_b.jpg" alt="Cómo usar benchmarking para mejorar el servicio al cliente">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-4" href="http://www.gestiopolis.com/marketing-2/como-usar-benchmarking-para-mejorar-servicio-cliente.htm" title="Cómo usar benchmarking para mejorar el servicio al cliente" rel="bookmark"><span>Cómo usar benchmarking para mejorar el servicio al cliente</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-3"><i class="fa icon-cat-3"></i></span>
+                <div class="destacado-content cat-bg-3">
+                  <div class="meta"><i class="fa fa-eye"></i> 23 <i class="fa fa-heart"></i> 5 <i class="fa fa-comment"></i> 0</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/marketing-2/como-usar-benchmarking-para-mejorar-servicio-cliente.htm">¿Te ha pasado alguna vez de probar un muy buen servicio en una empresa nueva, y
+                  luego ver cómo ese nivel de servicio decae y se equipara a la mediocridad que lo rodea? ¿Se trata sólo de un muy buen primer impulso, que después es difícil de mantener? En este artículo quiero mostrarte cómo puedes usar el benchmarking para mantener un alto nivel de servicio.</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Mariana Pizzo
+                  </a>
+                  <div class="botones-compartir" id="compartir-4">
+                    <div class="platform bc-facebook" id="fb-compartir-4"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-4"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-4"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-4"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-36 -->
+          </div><!-- .row -->
+        </div><!-- .span6 -->
+        <div class="col-sm-9">
+          <div class="row rm8">
+            <div class="col-sm-36">
+              <article id="post-5" class="destacado-5 post">
+                <img src="http://farm3.staticflickr.com/2851/9079538940_9bb59aff6e_b.jpg" alt="Los emprendedores tienen miedo a arruinarse">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-5" href="http://www.gestiopolis.com/innovacion-emprendimiento-2/emprendedores-tienen-miedo-arruinarse.htm" title="Los emprendedores tienen miedo a arruinarse" rel="bookmark"><span>Los emprendedores tienen miedo a arruinarse</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-18"><i class="fa icon-cat-18"></i></span>
+                <div class="destacado-content cat-bg-18">
+                  <div class="meta"><i class="fa fa-eye"></i> 28 <i class="fa fa-heart"></i> 9 <i class="fa fa-comment"></i> 1</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/innovacion-emprendimiento-2/emprendedores-tienen-miedo-arruinarse.htm">Una de las características para ser empresarios es la de asumir riesgos, el riesgo de perder el capital que haya invertido en el proyecto, o tener que devolver los prestamos que le han hecho, sin que tenga éxito su empresa, haciendo frente a los mismos con su patrimonio, o en muchas ocasiones, con parte del sueldo que recibe al haber tenido que ponerse a trabajar...</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Antonio Avilés Caballero
+                  </a>
+                  <div class="botones-compartir" id="compartir-5">
+                    <div class="platform bc-facebook" id="fb-compartir-5"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-5"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-5"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-5"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-36 -->
+          </div><!-- .row -->
+          <div class="row rm8">
+            <div class="col-sm-36">
+              <article id="post-6" class="destacado-6 post">
+                <img src="http://farm4.staticflickr.com/3728/9079538906_f44d205f94_b.jpg" alt="Mercadeo interno en la motivación y la productividad de los empleados">
+                <div class="overlay"></div>
+                <h2 class="entry-title"><a id="titulo-6" href="http://www.gestiopolis.com/organizacion-talento-2/mercadeo-interno-en-la-motivacion-y-la-productividad-de-los-empleados.htm" title="Mercadeo interno en la motivación y la productividad de los empleados" rel="bookmark"><span>Mercadeo interno en la motivación y la productividad de los empleados</span></a></h2>
+                <span class="compartir"><i class="fa fa-share"></i></span>
+                <span class="cat-bg-3"><i class="fa icon-cat-3"></i></span>
+                <div class="destacado-content cat-bg-3">
+                  <div class="meta"><i class="fa fa-eye"></i> 19 <i class="fa fa-heart"></i> 46 <i class="fa fa-comment"></i> 15</div>
+                  <div class="excerpt"><a href="http://www.gestiopolis.com/organizacion-talento-2/mercadeo-interno-en-la-motivacion-y-la-productividad-de-los-empleados.htm">Así como en la relación con los clientes, las empresas deben tener como objetivo contar con estándares altos de calidad en su relación con los empleados, logrando que ellos se sientan positivamente conectados con la compañía. Un empleado feliz, motivado y contento se traduce en mayor productividad y por lo tanto mejores resultados corporativos.</a></div>
+                  <a class="autor" href="#">
+                    <img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=21" class="avatar avatar-21 avatar-default" height="21" width="21" style="width: 21px; height: 21px;" alt="avatar"> Por: Daniel Diaz Jiménez
+                  </a>
+                  <div class="botones-compartir" id="compartir-6">
+                    <div class="platform bc-facebook" id="fb-compartir-6"></div>
+                    <div class="platform bc-twitter" id="tweet-compartir-6"></div>
+                    <div class="platform bc-linkedin" id="linkedin-compartir-6"></div>
+                    <div class="platform bc-gplus" id="gplus-compartir-6"></div>
+                  </div>
+                </div>
+              </article>
+            </div><!-- .col-sm-36 -->
+          </div><!-- .row -->
+        </div><!-- .span3 -->
+      </div><!-- .row -->
+    </div>
+  </div><!-- .row DESTACADOS -->
+</div><!-- .container.cm8 -->
+<?php } else { ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  algo va por aquí
-  <?php get_template_part('templates/content', get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php if ($wp_query->max_num_pages > 1) : ?>
-  <nav class="post-nav">
-    <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-    </ul>
-  </nav>
-<?php endif; ?>
+<?php } ?>
