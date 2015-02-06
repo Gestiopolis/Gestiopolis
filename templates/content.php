cbe3367037339fb9bd9d35eb68b7fc4f8110520f
Diego Castillo <djdiego88@gmail.com>
2015-02-06 15:47:41 -0500

    Changed styles in list of posts

--- templates/content.php
+++ templates/content.php
@@ -22,21 +22,28 @@
         <a class="autor" href="<?php echo get_author_posts_url($post->post_author); ?>">
           <?php echo get_author_color_id(); ?> 
           <?php if(get_post_meta($post->ID, "author-name_value", true) != "") : ?>
-          Por: <?php echo get_the_author();//echo get_post_meta($post->ID, "author-name_value", true); ?>
+          <strong><?php echo get_the_author();//echo get_post_meta($post->ID, "author-name_value", true); ?></strong>
           <?php else : ?>
-          Por: <?php echo get_the_author(); ?>
+          <strong><?php echo get_the_author(); ?></strong>
           <?php endif; ?>
         </a>
-        <div class="fecha"><?php echo get_the_date('j\.m\.Y'); ?></div>
         <div class="post-content">
           <p><?php echo title_trim(220, get_the_excerpt()); ?></p>
         </div>
+        <div class="tiempo-fecha">
+          <div class="tiempo pull-left"><?php echo estimate_time();?> de lectura</div>
+          <div class="fecha pull-right"><?php echo get_the_date('j\.m\.Y'); ?></div>
+        </div>
+        <div class="category pull-left"><a href="#" class="cat-bg-<?php echo $category[0]->term_id; ?>"><i class="fa icon-cat-<?php echo $category[0]->term_id; ?>"></i> <?php echo $category[0]->cat_name; ?></a></div>
+        <?php the_tags('<div class="tags"><i class="fa fa-tags"></i> ',', ','</div>'); ?>
       </div>
     </div>
-    <div class="wrapper-meta clearfix">
+    <!--<div class="wrapper-meta clearfix">
+      <div class="tiempo pull-left"><?php echo estimate_time();?> de lectura</div>
+      <div class="fecha pull-right"><?php echo get_the_date('j\.m\.Y'); ?></div>
       <?php the_tags('<div class="tags"><i class="fa fa-tags"></i> ',', ','</div>'); ?>
       <div class="stats"><i class="fa fa-eye"></i> <?php if(function_exists('the_views')) { the_views(); } ?> <i class="fa fa-comments"></i> <?php comments_number('0','1','%'); ?> <i class="fa fa-heart"></i> 21</div>
-      <div class="tiempo"><i class="fa fa-coffee"></i> Leerlo te tomará <?php echo estimate_time();?></div>
-    </div>
+      
+    </div>-->
   </article><!-- .post -->
 </div><!-- .col-md-3 col-sm-6 -->
