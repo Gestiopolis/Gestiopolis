d6c60a68bfff772a285d709f6342c735e2e7d556
Diego Castillo <djdiego88@gmail.com>
2015-02-06 18:33:42 -0500

    Simplified sidebar post

--- templates/sidebar-post.php
+++ templates/sidebar-post.php
@@ -33,79 +33,9 @@
     <a href="#" class="popular"><i class="fa fa-thumbs-up"></i> Populares</a>
   </div><!-- eje tabs -->
   <div class="sidebar-post">
-    <?php $args1=array( 'cat' => $category_id, 'posts_per_page'=>2, 'post__not_in'=>array($post->ID));//Empieza query del último post
+    <?php $args1=array( 'cat' => $category_id, 'posts_per_page'=>9, 'post__not_in'=>array($post->ID));//Empieza query del último post
       $query1 = new WP_Query($args1);
         if( $query1->have_posts() ) { while ($query1->have_posts()) : $query1->the_post(); ?>
-    <article id="post-<?php echo $post->ID; ?>" class="post">
-      <?php echo wp_imager(640, 360, '', 'img-responsive'); ?>
-      <div class="overlay"></div>
-      <h3 class="entry-title"><a id="titulo-<?php echo $post->ID; ?>" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><span><?php the_title_attribute(); ?></span></a></h3>
-      <div class="destacado-content cat-bg-<?php echo $category_id; ?>">
-        <div class="botones-compartir" id="compartir-<?php echo $post->ID; ?>">
-          <div class="platform bc-facebook" id="fb-compartir-<?php echo $post->ID; ?>"></div>
-          <div class="platform bc-twitter" id="tweet-compartir-<?php echo $post->ID; ?>"></div>
-          <div class="platform bc-linkedin" id="linkedin-compartir-<?php echo $post->ID; ?>"></div>
-        </div>
-      </div>
-    </article>
-    <?php endwhile;?>
-    <?php } 
-    wp_reset_query(); 
-    wp_reset_postdata(); ?>
-    <div class="autores-sidebar">
-      <h3>Más autores en <?php echo $category_name; ?></h3>
-      <div class="wrapper-nombre">
-        <a href="#"><img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=56" class="avatar avatar-56 avatar-default" height="56" width="56" style="width: 56px; height: 56px;" alt="avatar"></a>
-        <a href="#"><img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=56" class="avatar avatar-56 avatar-default" height="56" width="56" style="width: 56px; height: 56px;" alt="avatar"></a>
-        <a href="#"><img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=56" class="avatar avatar-56 avatar-default" height="56" width="56" style="width: 56px; height: 56px;" alt="avatar"></a>
-        <a href="#"><img src="http://gravatar.com/avatar/e71197281d0838afcc0a1f838e78441f?s=56" class="avatar avatar-56 avatar-default" height="56" width="56" style="width: 56px; height: 56px;" alt="avatar"></a>
-        <a href="#" class="prev"><i class="fa icon-chevron-left"></i></a>
-        <a href="#" class="next"><i class="fa icon-chevron-right"></i></a>
-      </div>
-      <div class="nombre-titu">
-        <div class="nombre-autor"><a href="#">Elisabeth Judson Shue Guggenheim</a></div>
-        <div class="titular-autor">Actriz de cine y televisión. Nominada a los Oscar, Golden Globe, BAFTA y SAG</div>
-      </div>
-      <div class="wrapper-meta">
-        <div class="span1">
-          <span class="number">12</span> publicaciones
-        </div><!-- .span1 -->
-        <div class="span1">
-          <span class="number">123456</span> lectores
-        </div><!-- .span1 -->
-        <!--<div class="span1">
-          <a href="#" class="btn btn-seguir"><i class="fa fa-plus-sign"></i> Seguir</a>
-        </div>--><!-- .span1 -->
-      </div>
-    </div>
-    <?php $args2=array( 'cat' => $category_id, 'posts_per_page'=>3, 'post__not_in'=>array($post->ID), 'offset' => 2);//Empieza query del último post
-      $query2 = new WP_Query($args2);
-        if( $query2->have_posts() ) { while ($query2->have_posts()) : $query2->the_post(); ?>
-    <article id="post-<?php echo $post->ID; ?>" class="post">
-      <?php echo wp_imager(640, 360, '', 'img-responsive'); ?>
-      <div class="overlay"></div>
-      <h3 class="entry-title"><a id="titulo-<?php echo $post->ID; ?>" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><span><?php the_title_attribute(); ?></span></a></h3>
-      <div class="destacado-content cat-bg-<?php echo $category_id; ?>">
-        <div class="botones-compartir" id="compartir-<?php echo $post->ID; ?>">
-          <div class="platform bc-facebook" id="fb-compartir-<?php echo $post->ID; ?>"></div>
-          <div class="platform bc-twitter" id="tweet-compartir-<?php echo $post->ID; ?>"></div>
-          <div class="platform bc-linkedin" id="linkedin-compartir-<?php echo $post->ID; ?>"></div>
-        </div>
-      </div>
-    </article>
-    <?php endwhile;?>
-    <?php } 
-    wp_reset_query(); 
-    wp_reset_postdata(); ?>
-    <div class="temas-sidebar">
-      <h3>Temas tendencia en <?php echo $category_name; ?></h3>
-      <div class="wrapper-temas">
-        <?php popular_tags_from_category($category_id, 1240, 8)?>
-      </div>
-    </div>
-    <?php $args3=array( 'cat' => $category_id, 'posts_per_page'=>4, 'post__not_in'=>array($post->ID), 'offset' => 5);//Empieza query del último post
-      $query3 = new WP_Query($args3);
-        if( $query3->have_posts() ) { while ($query3->have_posts()) : $query3->the_post(); ?>
     <article id="post-<?php echo $post->ID; ?>" class="post">
       <?php echo wp_imager(640, 360, '', 'img-responsive'); ?>
       <div class="overlay"></div>
