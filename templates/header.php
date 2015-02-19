3ff02dc9942c7febc711df7a3fc30c2d9be27593
Diego Castillo <djdiego88@gmail.com>
2015-02-19 14:43:21 -0500

    Test without navigation

--- templates/header.php
+++ templates/header.php
@@ -44,9 +44,9 @@
       <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img width="179" height="48" class="logo" src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/logo.png'); ?>" alt="<?php bloginfo('name'); ?>"></a>
     </div>
 
-    <nav role="navigation">
+    <!--<nav role="navigation">
       <ul class="nav navbar-nav navbar-right">
-        <?php if(!is_single()){ ?>
+        <?php if(!is_single()){ ?>-->
         <!--<li id="nav_explora" class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="drop_explora"><i class="fa fa-bars"></i> Explora <i class="fa fa-sort-desc"></i></a>
           <div id="menu_explora" class="dropdown-menu" role="menu" aria-labelledby="drop_explora">
@@ -83,7 +83,7 @@
           </div>
         </li>-->
         <?php } ?>
-        <li id="nav_publica"><a href="<?php echo get_page_link(264); ?>"><i class="fa fa-cloud-upload"></i><span class="hidden-xs"> Publica</span></a></li>
+        <!--<li id="nav_publica"><a href="<?php echo get_page_link(264); ?>"><i class="fa fa-cloud-upload"></i><span class="hidden-xs"> Publica</span></a></li>
         <li id="nav_busca">
           <div id="sb-search" class="sb-search">
             <form id="searchbox_002900072100095058217:mp7ncjp0apo" action="<?php echo home_url( '/' ); ?>" role="search">
@@ -92,7 +92,6 @@
               <input type="hidden" name="ie" value="UTF-8">
               <input class="sb-search-input" placeholder="Ingresa tu búsqueda..." type="search" value="" name="s" id="search">
               <input class="sb-search-submit" type="submit" value="">
-              <!--<i class="fa fa-search sb-icon-search"></i>-->
               <span class="sb-icon-search"><i class="fa fa-search"></i><span class="hidden-xs"> Busca</span></span>
             </form>
           </div>
@@ -104,6 +103,6 @@
           </a>
         </li>
       </ul>
-    </nav>
+    </nav>-->
   </div>
 </header>
