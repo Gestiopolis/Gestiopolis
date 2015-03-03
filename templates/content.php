2e558ff7bee02ac8a70ab3c96b4406d82680e86a
Diego Castillo <djdiego88@gmail.com>
2015-03-03 14:54:35 -0500

    Fixed image responsive

--- templates/content.php
+++ templates/content.php
@@ -5,7 +5,7 @@
   <article id="post-<?php the_ID(); ?>" class="post">
     <div class="wrapper-img">
       <a href="<?php echo get_permalink($post->ID); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
-        <img class="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/grey.gif" data-original="<?php echo wp_imager(640, 360, '', 'img-responsive', false, null, true); ?>" alt="<?php the_title_attribute(); ?>" class="img-responsive">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/grey.gif" data-original="<?php echo wp_imager(640, 360, '', 'img-responsive', false, null, true); ?>" alt="<?php the_title_attribute(); ?>" class="lazy img-responsive">
         <div class="overlay"></div>
         <div class="vert-center-wrapper">
           <div class="vert-centered">
