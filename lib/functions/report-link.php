db588b0fb02872dc28862cc37ad836a56452ae26
Diego Castillo <djdiego88@gmail.com>
2015-03-05 12:05:58 -0500

    Testing email type html in link report

--- lib/functions/report-link.php
+++ lib/functions/report-link.php
@@ -8,8 +8,6 @@ $title = ( isset($_POST['title']) && (string)$_POST['title'] ) ? $_POST['title']
 $url = ( isset($_POST['url']) && (string)$_POST['url'] ) ? $_POST['url'] : false;
 if($linkid && $post_ID && $title && $url){
 
-	add_filter( 'wp_mail_content_type', 'set_html_content_type' );
-
 	$to = 'djdiego88@gmail.com';
 	$subject = 'Enlace reportado roto en GestioPolis';
 	$body = '<h1>Reporte de enlace roto</h1>';
@@ -17,22 +15,11 @@ if($linkid && $post_ID && $title && $url){
 	$body .= '<p>Título del enlace: '.$title.'</p>';
 	$body .= '<p>URL del enlace: '.$url.'</p><br>';
 	$body .= '<p>Gracias por su atenci&oacute;n.</p>';
-	$headers = array('Content-Type: text/html; charset=UTF-8');
-
+	$headers[] = 'From: Gestiopolis <info@gestiopolis.com>';
+	$headers[] = 'Content-Type: text/html; charset=UTF-8';
+	
 	$send = wp_mail( $to, $subject, $body, $headers );
 
-	// Reset content-type to avoid conflicts -- http://core.trac.wordpress.org/ticket/23578
-	remove_filter( 'wp_mail_content_type', 'set_html_content_type' );
-
-	function set_html_content_type() {
-		return 'text/html';
-	}
-	$log = ($send == true) ? "wp_mail: true\n" : "wp_mail: false\n";
-	$log .= "linkid: '.$linkid.'\n";
-	$log .= "pid: '.$post_ID.'\n";
-	$log .= "title: '.$title.'\n";
-	$log .= "url: '.$url.'\n";
-	return $log;
 }else{
 	echo 'No deber&iacute;as estar aqu&iacute;';
 }
