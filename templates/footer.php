fcc51cbd2eb4ae807e8f1268abc1cd24117ae3af
Diego Castillo <djdiego88@gmail.com>
2015-04-18 12:15:27 -0500

    Added footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,15 @@
 <div class="footer">
   <div class="footer-inner">
     <footer class="container">
-      footer
+      <ul class="list-unstyled">
+      	<li><a href="#">Inicio</a></li>
+      	<li><a href="#">Acerca de</a></li>
+      	<li><a href="#">ABC Temático</a></li>
+      	<li><a href="#">Archivo</a></li>
+      	<li><a href="#">Contacto</a></li>
+      	<li><a href="#">Términos de Uso</a></li>
+      	<li>&copy; 2000 - <?php echo date('Y');?> WebProfit Ltda.</li>
+      </ul>
     </footer>
   </div>
 </div>
