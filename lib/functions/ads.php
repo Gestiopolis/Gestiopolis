b8f0f2f2f6e63a07aa6802ec8a65267f0341987b
Diego Castillo <djdiego88@gmail.com>
2015-05-06 19:58:23 -0500

    Added ads for first paragraph in test post

--- lib/functions/ads.php
+++ lib/functions/ads.php
@@ -1,14 +1,14 @@
 <?php
  
-//Insert ads after second paragraph of single post content.
 
+//Isserta admanmedia ads después del primer párrafo
 add_filter( 'the_content', 'insert_adman_ads' );
 
 function insert_adman_ads( $content ) {
 	
 	$ad_code = '<div id="admanmedia"><script src="http://icarus-wings.admanmedia.com/intext/intext_vast.js?pmu=183f9431;pmb=216f0476;size=600x338;visibility=50"></script></div>';
 
-	if ( is_single() && ! is_admin() ) {
+	if ( is_single(332811) && ! is_admin() ) {
 		return prefix_insert_after_paragraph( $ad_code, 1, $content );
 	}
 	
