10830751270e910df55761a5a7452b444a44e757
Diego Castillo <djdiego88@gmail.com>
2015-06-03 13:40:22 -0500

    Fixed admanmedia without DFP

--- lib/functions/ads.php
+++ lib/functions/ads.php
@@ -10,12 +10,7 @@ add_filter( 'the_content', 'insert_adman_ads' );
 function insert_adman_ads( $content ) {
     global $post;
 	
-	$ad_code = '<div id="admanmedia" class="hidden-xs"><!-- /1007663/post-3er-parrafo-contenido -->
-<div id=\'div-gpt-ad-1433303077158-0\'>
-<script type=\'text/javascript\'>
-googletag.cmd.push(function() { googletag.display(\'div-gpt-ad-1433303077158-0\'); });
-</script>
-</div></div>';
+	$ad_code = '<div id="admanmedia" class="hidden-xs"><script src="http://icarus-wings.admanmedia.com/intext/intext_vast.js?pmu=183f9431;pmb=216f0476;size=600x338;visibility=50"></script></div>';
 
 	if ( is_single() && ! is_admin() && get_post_meta($post->ID, "all2html_htmlcontent", true) == "" ) {
 		return prefix_insert_after_paragraph( $ad_code, 3, $content );
