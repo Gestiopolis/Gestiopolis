fb8ebd4bb49d8686241e7cb53af2f5d0a8974c7f
Diego Castillo <djdiego88@gmail.com>
2015-06-02 20:45:36 -0500

    Testing DFP mobile Ads

--- lib/extras.php
+++ lib/extras.php
@@ -1287,19 +1287,7 @@ add_action('wp_footer', 'footer_dataxpand', 100);
 
 function head_scripts_ads() {
 	if(is_single()) {
-    echo '
-    <!-- Hotjar Tracking Code for www.gestiopolis.com -->
-<script>
-    (function(f,b){
-        var c;
-        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
-        f._hjSettings={hjid:5254, hjsv:4};
-        c=b.createElement("script");c.async=1;
-        c.src="//static.hotjar.com/c/hotjar-"+f._hjSettings.hjid+".js?sv="+f._hjSettings.hjsv;
-        b.getElementsByTagName("head")[0].appendChild(c); 
-    })(window,document);
-</script>
-	<script type=\'text/javascript\'>
+    echo '<script type=\'text/javascript\'>
   var googletag = googletag || {};
   googletag.cmd = googletag.cmd || [];
   (function() {
