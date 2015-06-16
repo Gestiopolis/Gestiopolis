7a7077c105dac97a6c79c72c1a7bd1aa14e229f4
Diego Castillo <djdiego88@gmail.com>
2015-06-16 14:46:30 -0500

    Change to single

--- templates/content-single.php
+++ templates/content-single.php
@@ -23,7 +23,7 @@
     <div class="row">
       <div class="col-sm-12 col-md-9 maincol">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <h1 class="entry-title title"><?php the_title(); ?></h1>
           <div class="row"><!-- Empieza row de contenido y meta datos -->
             <div class="col-sm-12 col-md-2">
               <div class="breadcredit">
