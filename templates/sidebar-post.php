8281905106c072091b7ebc0557b2aec71bb5ca2c
Diego Castillo <djdiego88@gmail.com>
2015-07-14 15:05:51 -0500

    Testing sidebar full

--- templates/sidebar-post.php
+++ templates/sidebar-post.php
@@ -3,10 +3,53 @@
   <div class="sidebar-post">
     <h3>Más populares</h3>
     <i class="fa fa-caret-down"></i>
+    <?php $tposts = get_trending_posts(10, TRENDING_DAYS);
+      $i = 1;
+      foreach ($tposts as $tpost) {
+        $post_title = stripslashes($tpost->post_title);
+        $permalink = get_permalink($tpost->ID);
+        $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $tpost->ID ), 'thumbnail' );
+    ?>
+    <article id="post-<?php echo $tpost->ID; ?>" class="post">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/grey.gif" data-original="<?php echo $large_image_url[0]; ?>" alt="<?php echo $post_title; ?>" class="lazy pull-left" width="64" height="56">
+      <div class="wrapper-content">
+        <h2 class="entry-title"><a href="<?php echo $permalink; ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php echo $post_title; ?></a></h2>
+        <?php 
+        $categories = get_the_category($tpost->ID); 
+        foreach($categories as $category){
+          echo '<i class="fa icon-cat-'.$category->term_id.' cat-col-'.$category->term_id.'"></i> <a class="cat-col-'.$category->term_id.'" href="'.get_category_link($category->term_id ).'">'.$category->cat_name.'</a> &nbsp;';
+        }
+        ?>
+      </div>
+    </article>
+    <?php } // fin foreach $tposts ?>
   </div>
   <div class="sidebar-post">
     <h3>Más recientes</h3>
     <i class="fa fa-caret-down"></i>
+    <?php 
+    $query2 = new WP_Query('posts_per_page=6');
+    while ( $query2->have_posts() ) {
+      $query2->the_post(); ?>
+      <article id="post-<?php the_ID(); ?>" class="post">
+      <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+      ?>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/grey.gif" data-original="<?php echo $large_image_url[0]; ?>" alt="<?php the_title_attribute(); ?>" class="lazy pull-left" width="64" height="56">
+      <div class="wrapper-content">
+        <h2 class="entry-title"><a href="<?php echo get_permalink($post->ID); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <?php 
+        $categories = get_the_category($post->ID); 
+        foreach($categories as $category){
+          echo '<i class="fa icon-cat-'.$category->term_id.' cat-col-'.$category->term_id.'"></i> <a class="cat-col-'.$category->term_id.'" href="'.get_category_link($category->term_id ).'">'.$category->cat_name.'</a> &nbsp;';
+        }
+        ?>
+      </div>
+    </article>
+    <?php 
+    }
+    wp_reset_query(); 
+    wp_reset_postdata();
+    ?>
   </div>
   <div class="sidebar-post">
     <h3>Vas a querer leer</h3>
@@ -31,7 +74,7 @@
         <?php 
         $categories = get_the_category($post->ID); 
         foreach($categories as $category){
-          echo '<i class="fa icon-cat-'.$category->term_id.' cat-col-'.$category->term_id.'"></i> <a class="cat-col-'.$category->term_id.'" href="'.get_category_link($category->term_id ).'">'.$category->cat_name.'</a> &nbsp;&nbsp;';
+          echo '<i class="fa icon-cat-'.$category->term_id.' cat-col-'.$category->term_id.'"></i> <a class="cat-col-'.$category->term_id.'" href="'.get_category_link($category->term_id ).'">'.$category->cat_name.'</a> &nbsp;';
         }
         ?>
       </div>
