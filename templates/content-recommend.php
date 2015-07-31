3e71f49ab738d0b56f4e9fa797c54ed17fc05e96
Diego Castillo <djdiego88@gmail.com>
2015-07-31 10:35:47 -0500

    Activated agind the related posts section in mobile

--- templates/content-recommend.php
+++ templates/content-recommend.php
@@ -3,7 +3,7 @@
     <div class="wrapper-img">
       <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'main-thumb' );
       ?>
-      <a href="<?php echo get_permalink($post->ID); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+      <a href="<?php echo get_permalink($post->ID); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" data-ic="ic_medium=popular_posts&ic_source=mobile-related" class="internal-campaign">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/grey.gif" data-original="<?php echo $large_image_url[0]; ?>" alt="<?php the_title_attribute(); ?>" class="lazy img-responsive">
         <div class="overlay"></div>
         <div class="vert-center-wrapper">
--- templates/content-single.php
+++ templates/content-single.php
@@ -288,6 +288,37 @@ src="//pagead2.googlesyndication.com/pagead/show_ads.js">
               <h2><i class="fa fa-tags"></i> En este post se habla sobre</h2>
               <?php the_tags('<div class="temas-archive"> ','','</div>'); ?>
             </div><!-- .post-tags -->
+            <div class="compartelo posts-home hidden-md hidden-lg">
+              <div class="title-section"><h2>Te recomendamos</h2><i class="fa fa-caret-down"></i></div>
+              <?php 
+              $show = 12;
+              $postsnot = array();
+              $postsnot[] = $post->ID;
+              $mainpost = $post->ID;
+              $query1 = ci_get_related_posts_1( $post->ID, $show );
+              //$countp = 1;
+                  if( $query1->have_posts() ) { while ($query1->have_posts()) : $query1->the_post(); 
+                    $postsnot[] = $post->ID;
+                    if($mainpost != $post->ID){
+                      get_template_part( 'templates/content', 'recommend' );
+                    }
+                    //$countp++; 
+                   endwhile;?>
+                  <?php } 
+                  wp_reset_query(); 
+                  wp_reset_postdata(); 
+                  $show = $show - count($query1->posts);
+                 if ($show > 0) {
+                  $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+                $query2 = ci_get_related_posts_2( $post->ID, $postsnot, $show, $paged );
+                    if( $query2->have_posts() ) { while ($query2->have_posts()) : $query2->the_post();get_template_part( 'templates/content', 'recommend' );
+                     endwhile;
+                    } 
+                    wp_reset_query(); 
+                    wp_reset_postdata();
+                  }
+                  ?>
+            </div><!-- .recomendados -->
             <?php if(is_user_logged_in() && current_user_can( 'manage_options')){ ?>
             <?php get_template_part('templates/post-front-edit'); ?>
             <?php } ?>
