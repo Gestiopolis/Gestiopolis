543f26f7aeab6d31ceb4a23194997132878906fe
Diego Castillo <djdiego88@gmail.com>
2015-08-05 22:18:30 -0500

    Changed styles in vote and behavior of trending authors

--- templates/content-single.php
+++ templates/content-single.php
@@ -377,7 +377,7 @@ src="//pagead2.googlesyndication.com/pagead/show_ads.js">
     update_post_meta($post->ID, '_liked', 1);
   }
   if(has_tag(8175)){
-    echo do_shortcode("[pro_ad_display_adzone id='325453' flyin='1' flyin_delay='3']"); 
+    echo do_shortcode("[pro_ad_display_adzone id='334406' flyin='1' flyin_delay='3']"); 
   }
   ?>
 <?php endwhile; ?>
