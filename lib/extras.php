9e572881989bb3d48f15b098b59213192841e4f9
Diego Castillo <djdiego88@gmail.com>
2015-09-17 09:48:55 -0500

    Fixed DFP for Billymob AD

--- lib/extras.php
+++ lib/extras.php
@@ -1470,19 +1470,24 @@ function head_scripts_ads() {
 </script>';*/
 echo '
 <script type=\'text/javascript\'>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
   (function() {
+    var gads = document.createElement(\'script\');
+    gads.async = true;
+    gads.type = \'text/javascript\';
     var useSSL = \'https:\' == document.location.protocol;
-    var src = (useSSL ? \'https:\' : \'http:\') +
-        \'//www.googletagservices.com/tag/js/gpt.js\';
-    document.write(\'<scr\' + \'ipt src="\' + src + \'"></scr\' + \'ipt>\');
+    gads.src = (useSSL ? \'https:\' : \'http:\') +
+      \'//www.googletagservices.com/tag/js/gpt.js\';
+    var node = document.getElementsByTagName(\'script\')[0];
+    node.parentNode.insertBefore(gads, node);
   })();
 </script>
 <script type=\'text/javascript\'>
   googletag.cmd.push(function() {
-    googletag.defineSlot(\'/1007663/zocalo-movil\', [300, 125], \'div-gpt-ad-1442329714332-0\').addService(googletag.pubads());
+    googletag.defineSlot(\'/1007663/zocalo-movil\', [300, 125], \'div-gpt-ad-1442500998553-0\').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
-    googletag.pubads().enableSyncRendering();
     googletag.enableServices();
   });
 </script>
