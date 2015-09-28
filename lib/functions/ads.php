47bed91d22f589316f8e79427de33ff119c81522
Diego Castillo <djdiego88@gmail.com>
2015-09-28 11:34:15 -0500

    Moified weights of admand, tead and despegar ads

--- lib/functions/ads.php
+++ lib/functions/ads.php
@@ -43,8 +43,8 @@ js.src = \'//cdn.teads.tv/media/format.js\';
 s.parentNode.insertBefore(js, s);
 })(window.document);
 </script></div>';
-$anuncios = array($despegar,$adman,$teads);
-$ad_code = $anuncios[rand(0,2)];
+$anuncios = array($despegar,$adman,$teads,$adman,$adman);
+$ad_code = $anuncios[rand(0,4)];
 
 	if ( is_single() && ! is_admin() && get_post_meta($post->ID, "all2html_htmlcontent", true) == "" ) {
         //if(!is_single(28207 )){
