03ed4aba363437e676a0547d7926c61833bdf3ef
Diego Castillo <djdiego88@gmail.com>
2016-04-06 21:13:56 -0500

    Fixed DFP

--- lib/functions/ads.php
+++ lib/functions/ads.php
@@ -564,7 +564,7 @@ $dfpprincipal = "<script type='text/javascript'>
     var useSSL = 'https:' == document.location.protocol;
     var src = (useSSL ? 'https:' : 'http:') +
         '//www.googletagservices.com/tag/js/gpt.js';
-    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt src=\"' + src + '\"></scr' + 'ipt>');
   })();
 </script>
 
